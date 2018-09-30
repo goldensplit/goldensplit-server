82f98c5d0e16eb8e30093522b461257bcfe16dd1
voledemar <voledemar@inbox.ru>
2018-09-30 21:56:13 +0300

    #6 add models concept and provider mocks

---
+++ model/model.go
@@ -0,0 +1,40 @@
+// Package model provides TODO: Docs.
+package model
+
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
+
+// Model TODO: Docs.
+type Model interface {
+	mock()
+}
+
+// User TODO: Docs.
+type User struct {
+	gorm.Model
+	Name  string
+	Email string
+	Info  string
+}
+
+// Session TODO: Docs.
+type Session struct {
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt *time.Time
+}
+
+// Video TODO: Docs.
+type Video struct {
+	gorm.Model
+	UploaderID uint
+	Info       string
+	Tags       []string
+}
+
+func (User) mock()    {}
+func (Session) mock() {}
+func (Video) mock()   {}
---
+++ provider/provider.go
@@ -0,0 +1,29 @@
+// Package provider provides TODO: Docs.
+package provider
+
+import (
+	"github.com/goldensplit/goldensplit-server/model"
+)
+
+// Provider TODO: Docs.
+type Provider struct{}
+
+// Create  TODO: Docs.
+func (Provider) Create(model.Model) model.Model {
+	return nil
+}
+
+// Read  TODO: Docs.
+func (Provider) Read(model.Model) model.Model {
+	return nil
+}
+
+// Update  TODO: Docs.
+func (Provider) Update(model.Model) model.Model {
+	return nil
+}
+
+// Delete  TODO: Docs.
+func (Provider) Delete(model.Model) model.Model {
+	return nil
+}
